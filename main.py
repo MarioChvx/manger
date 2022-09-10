1afc37a8a5825e4f0e9565ad3d73047fe6f3e1b2
Mario <mariochavez1292@gmail.com>
2022-09-09 19:20:08 -0500

    main

--- main.py
+++ main.py
@@ -3,26 +3,29 @@ from bs4 import BeautifulSoup
 import os
 
 def imageDownload(url,folder):
-   path = os.getcwd()
-   os.mkdir(os.path.join(path, folder))
-   os.chdir(os.path.join(path, folder))
-   r = requests.get(url)
-   soup = BeautifulSoup(r.text, 'html.parser')
+    path = os.getcwd()
+    os.mkdir(os.path.join(path, folder))
+    os.chdir(os.path.join(path, folder))
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
    
-   images = soup.find_all('img')
+    images = soup.find_all('img')
    
-   for image in images:
-      link = image['src']
-      name = image['alt']
-      if (link.startswith("https://")):
-         with open(name + '.png','wb') as f:
-            im = requests.get(link)
-            f.write(im.content)
-   os.chdir(path)
+    for image in images:
+        link = image['src']
+        name = image['alt']
+        if (link.startswith("https://")):
+            with open(name + '.png','wb') as f:
+                im = requests.get(link)
+                f.write(im.content)
+    os.chdir(path)
 
-url='https://www4.mymangalist.org/chapter-shuumatsu-no-valkyrie-'
-caps = range(6,62)
 
-for cap in caps:
-   imageDownload(url+str(cap),'Chapter'+str(cap))
+def main():
+    url='https://www4.mymangalist.org/chapter-shuumatsu-no-valkyrie-'
+    caps = range(6,62)
+    for cap in caps:
+        imageDownload(url+str(cap),'Chapter'+str(cap))
 
+if __name__ == '__main__':
+    main()
