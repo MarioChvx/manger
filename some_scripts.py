d4676a2b521347d72a54857892407759b6260478
Mario Chavez <mariochavea1292@gmail.com>
2022-11-13 08:04:49 -0600

    cli added

--- some_scripts.py
+++ some_scripts.py
@@ -2,15 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
+def get_img_urls(chapter_url:str):
+    """
+    Generates a list of strings with the URL's of the images
+    """
+
 def imageDownload(url,folder):
     path = os.getcwd()
     os.mkdir(os.path.join(path, folder))
     os.chdir(os.path.join(path, folder))
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-   
+
     images = soup.find_all('img')
-   
+
     for image in images:
         link = image['src']
         name = image['alt']
@@ -26,6 +31,3 @@ def main():
     caps = range(6,62)
     for cap in caps:
         imageDownload(url+str(cap),'Chapter'+str(cap))
-
-if __name__ == '__main__':
-    main()
