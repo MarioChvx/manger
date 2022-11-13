a63879e3029d22d081a3ad95d4ea4fcb353b922b
Mario Chavez <mairochavez1292@gmail.com>
2022-11-13 17:26:10 -0600

    actual cli added

---
+++ main.py
@@ -0,0 +1,13 @@
+import click
+
+@click.command()
+@click.option('--start-url', prompt='Provide the URL form the start chapter', help='The URL where the download is going to start')
+@click.option('--chapters', default=1, prompt='Provide number of chapters', help='The number of chapters is going to be downloaded')
+def manga_download(start_url, chapters):
+    """
+    Place holder
+    """
+    click.echo('Hello')
+
+if __name__ == '__main__':
+    manga_download()
