f50ee94985c01070bc5ac824e9505f8773685db2
Mario Chavez <mairochavez1292@gmail.com>
2022-11-19 15:55:04 -0600

    changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import click
 
+
 @click.command()
+@click.option('--manga-name', prompt='Provide the manga name', help='The title of the manga, is going to be used to name files')
 @click.option('--start-url', prompt='Provide the URL form the start chapter', help='The URL where the download is going to start')
 @click.option('--chapters', default=1, prompt='Provide number of chapters', help='The number of chapters is going to be downloaded')
 def manga_download(start_url, chapters):
