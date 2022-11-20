7a12e059d2279db2b5799c744992cef5e52fb877
Mario Chavez <mariochavez1292@gmail.com>
2022-11-19 18:52:36 -0600

    requirements.txt created

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import click
+import re
 
 
 @click.command()
 @click.option('--manga-name', prompt='Provide the manga name', help='The title of the manga, is going to be used to name files')
 @click.option('--start-url', prompt='Provide the URL form the start chapter', help='The URL where the download is going to start')
 @click.option('--chapters', default=1, prompt='Provide number of chapters', help='The number of chapters is going to be downloaded')
-def manga_download(start_url, chapters):
+def manga_download(start_url, chapters, manga_name):
     """
     Place holder
     """
     click.echo('Hello')
+    start_num = re.search('(?<=\[)(.*?)(?=\])', start_url)
 
 if __name__ == '__main__':
     manga_download()
