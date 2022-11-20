a0aac0e6297c6907e1d273ee95f78d125413d12c
Mario Chavez <mariochavez1292@gmail.com>
2022-11-19 18:52:36 -0600

    requirements.txt created

--- webscrap.py
+++ webscrap.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import os
 import re
 
-def get_img_urls(chapter_url:str):
-    """ Generates a list of strings with the URL's of the images
-    """
+def get_img_urls(soup):
+    """ Generates a list of strings with the URL's of the images """
+    images = soup.find_all('img')
+    links = [image['src'] for image in images]
+    return links
 
 def get_chapters_urls(chapter_url:str, n:int):
     """ Generates a list of urls
@@ -17,28 +18,9 @@ def get_chapters_urls(chapter_url:str, n:int):
     return urls
 
 
-def imageDownload(url,folder):
-    path = os.getcwd()
-    os.mkdir(os.path.join(path, folder))
-    os.chdir(os.path.join(path, folder))
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
-    images = soup.find_all('img')
-
-    for image in images:
-        link = image['src']
-        name = image['alt']
+def imageDownload(images_links):
+    for i, link in enumerate(images_links):
         if (link.startswith("https://")):
-            with open(name + '.png','wb') as f:
+            with open('.png','wb') as f:
                 im = requests.get(link)
                 f.write(im.content)
-    os.chdir(path)
-
-
-def main():
-    url='https://www4.mymangalist.org/chapter-shuumatsu-no-valkyrie-'
-    caps = range(6,62)
-    for cap in caps:
-        imageDownload(url+str(cap),'Chapter'+str(cap))
-
