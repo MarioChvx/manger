d4a22c8e05824a27a76434888eaca6f99ca9793d
Mario Chavez <mariochavez1292@gmail.com>
2023-08-02 15:08:14 -0600

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
+def gen_requirements():
+    with open('./requirements.txt', 'r') as file:
+        requirements = [line.strip() for line in file if line.strip()]
+    return requirements
+
 setup(
     name='manger',
     version='0.1.0',
-    py_modules=['manger'],
-    install_requires=[
-        'Click',
-        'BS4',
-        'Requests',
-        'Pillow'
+    author='Mario Chavez',
+    description='manger is a cli tool for manga downloading',
+    py_modules=[
+        'manger',
+        'manger_logic',
+        'directory_management',
+        'dir_to_pdf',
+        'web_scraping'
     ],
+    install_requires=gen_requirements(),
     entry_points={
         'console_scripts': [
             'manger = manger:manger',
